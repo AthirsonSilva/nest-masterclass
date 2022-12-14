20c1da6fa6e4230d266e709e928f5761e846f227
Athirson <athirsonarceus@gmail.com>
2022-12-14 20:48:55 -0300

    style: updating eslint and prettier config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common'
+import { team_member } from '@prisma/client'
+import { AppService } from './app.service'
+import { PrismaService } from './database/prisma.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(
+		private readonly appService: AppService,
+		private prisma: PrismaService,
+	) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	getHello(): string {
+		return this.appService.getHello()
+	}
+
+	@Post('store')
+	async createMember(): Promise<team_member> {
+		try {
+			return await this.prisma.team_member.create({
+				data: {
+					id: '1',
+					name: 'John Doe',
+					function: 'Programmer',
+				},
+			})
+		} catch (e) {
+			return e.message
+		}
+	}
 }
